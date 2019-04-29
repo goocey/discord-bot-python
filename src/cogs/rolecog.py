ab828a69c5e2ba94c87a672e3cea62205ea45cdf
luna <totugekiluna@gmail.com>
2019-04-29 20:27:09 +0900

    実装完了

--- src/cogs/rolecog.py
+++ src/cogs/rolecog.py
@@ -1,5 +1,6 @@
 from discord.ext import commands # Bot Commands Frameworkのインポート
 from lib.settings import settings
+from discord.ext.commands import errors
 import discord
 
 # コグとして用いるクラスを定義。
@@ -8,11 +9,7 @@ class RoleCog(commands.Cog):
     # TestCogクラスのコンストラクタ。Botを受取り、インスタンス変数として保持。
     def __init__(self, bot):
         self.bot = bot
-
-    # コマンドの作成。コマンドはcommandデコレータで必ず修飾する。
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.send('pong!')
+        self.role_list = settings.get_role_list()
 
     # メインとなるroleコマンド
     @commands.group()
@@ -24,19 +21,37 @@ class RoleCog(commands.Cog):
     # roleコマンドのサブコマンド
     # 指定したユーザーに指定した役職を付与する。
     @role.command()
-    async def add(self, ctx, member: discord.Member, role: discord.Role):
-        await member.add_roles(role)
+    async def add(self, ctx, role: discord.Role):
+        if str(role) not in self.role_list:
+            await ctx.send('指定可能なロール外です。')
+            return None
+
+        if role in ctx.guild.roles:
+            try:
+                await ctx.author.add_roles(role)
+            except errors.BadArgument:
+                await ctx.send('実際の役割とソース指定の役割が相違しているよ。')
 
     # roleコマンドのサブコマンド
-    # 指定したユーザーから指定した役職を剥奪する。
+    # 指定したユーザーに指定した役職を付与する。
     @role.command()
-    async def remove(self, ctx, member: discord.Member, role: discord.Role):
-        await member.remove_roles(role)
+    async def remove(self, ctx, role: discord.Role):
+        if str(role) not in self.role_list:
+            await ctx.send('指定可能なロール外です。')
+            return None
+
+        if role in ctx.guild.roles:
+            try:
+                await ctx.author.remove_roles(role)
+            except errors.BadArgument:
+                await ctx.send('実際の役割とソース指定の役割が相違しているよ。')
 
     @role.command()
-    async def help(self, ctx):
-        list_str = "\n".join(settings.get_role_list())
-        await ctx.send("役割一覧は以下の通りです。\n" + list_str)
+    async def help(self, ctx, *mes: str):
+        list_str = "\n".join(self.role_list)
+        message = ""
+        message = message + "役割一覧は以下の通りだよ。\n" + list_str
+        await ctx.send(message)
 
 # Bot本体側からコグを読み込む際に呼び出される関数。
 def setup(bot):
