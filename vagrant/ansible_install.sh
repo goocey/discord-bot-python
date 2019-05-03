2242461a0957c985794ee8705eb7949a8a4e50c9
luna <totugekiluna@gmail.com>
2019-05-04 03:28:30 +0900

    changed

--- vagrant/ansible_install.sh
+++ vagrant/ansible_install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 yum install -y ansible tmux
 echo "vagrant:vagrant" | chpasswd
+sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
